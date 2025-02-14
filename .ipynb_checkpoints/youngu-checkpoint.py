84e94f42103f2e88252cd3c89e9095deb358cc1c
park0609 <guppy135@naver.com>
2025-02-14 11:36:40 +0900

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/youngu-checkpoint.py
+++
@@ -1,8 +0,0 @@
-
-def youngu():
-    text = input("단어를 입력하세요")
-    if text[0] == text[2]:
-        return (f"{text}는 거꾸로 해도 같은 단어입니다.")
-    else:
-        return (f"{text}는 거꾸로 해도 같은 단어가 아닙니다.")
-youngu()

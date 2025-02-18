e421abd3c73c0292b0484584f34f1e292c4f2af0
park0609 <guppy135@naver.com>
2025-02-18 12:02:08 +0900

    Delete 0218 수업과제/.ipynb_checkpoints directory

--- 0218 수업과제/.ipynb_checkpoints/계산4-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import datetime
-def child():
-    cd = datetime.date(2025,5,5)
-    today = datetime.datetime.now().date()
-    diff = cd - today
-    days = ['월','화','수','목','금','토','일']
-    return f"오늘부터 어린이날까지는 {diff.days}일 남았으며, 어린이날은 {days[cd.weekday()]}요일 입니다."
-child()

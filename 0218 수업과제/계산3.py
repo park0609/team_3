21e1ea25543f464660a6251db6be5b3d3d09508e
jisangaaa <wltkd6881@naver.com>
2025-02-18 11:54:48 +0900

    Delete 0218 수업과제/계산3.py

--- 0218 수업과제/계산3.py
+++
@@ -1,8 +0,0 @@
-import datetime
-day1 = datetime.date(2025,2,18)
-day2 = datetime.date(2025,5,8)
-diff = day2 - day1 
-days=['월','화','수','목','금','토','일']
-day = datetime.date(2025,5,8)
-
-print("어버이날까지 남은 날은", diff.days,"일 이고,",days[day.weekday()]+'요일 입니다.')

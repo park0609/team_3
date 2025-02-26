2911203b1753f523351639c507d772d4fd27caf7
park0609 <guppy135@naver.com>
2025-02-26 16:41:47 +0900

    psc

--- 0220 팀과제/식단.py
+++ 0220 팀과제/식단.py
@@ -1,14 +1,33 @@
-import requests as req
-from bs4 import BeautifulSoup as bs
-url = "https://www.pusan.ac.kr/kor/CMS/MenuMgr/menuListOnBuilding.do?mCode=MN202"
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0"}
-web = req.get(url, headers = headers)
-soup = bs(web.content,"html5lib")
-menu_card = soup.select(".menu-tbl")
-dates = soup.select(".date") 
-days = soup.select(".day") 
-won = soup.select(".menu-tit01")
-menu = soup.select(".menu-tit01+p")
-for d, t, w, n in zip(dates, days, won,menu):
-    print("="*20)
-    print(d.text+" "+t.text+"\n"+"\n"+ w.text+"\n"+n.text)
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "77b36872-0597-44e4-b0ed-e7a1da9b24ab",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.1"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
